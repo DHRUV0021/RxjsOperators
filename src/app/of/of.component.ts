50471d55c802191293f95c92fa534927ff78e7b6
DHRUV0021 <gorasiyadhruv021@gmail.com>
2023-05-05 18:20:33 +0530

    SomeChanges

--- src/app/of/of.component.ts
+++ src/app/of/of.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs';
+import { of, interval, map, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-of',
@@ -8,9 +8,28 @@ import { of } from 'rxjs';
 })
 export class OfComponent {
 
-  source = of(1,2,3,4,5,6,7,8);  //this is a observable 
- of(){
-   const subscribe = this.source.subscribe(val => console.log(val));
- }
 
+  //================== OF OPRATOR  ===================//
+
+  source = of(1, 2, 3, 4, 5, 6, 7, 8);  //this is a observable 
+  of() {
+    const subscribe = this.source.subscribe(val => console.log(val));
+  }
+
+
+  //================== TAP/DO OPRATOR  ===================// 
+  test() {
+    const Arr = ['dhruv', 'krutik', 'akash', 'ankit', 'niraj', 'krushil'];
+    const source = interval(1500);
+    let obsSubscripation: Subscription;
+
+    obsSubscripation = source.pipe(
+      tap(res => {
+        if (res == 2) {
+          obsSubscripation.unsubscribe()
+        }
+      }),
+      map(res => Arr[res]))
+      .subscribe(res => console.log(res));
+  }
 }
